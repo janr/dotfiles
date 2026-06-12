0ef62eb730845dfa2c6be974c55ae2e5cb990d42
Jan Rudy <jan@rudy.rocks>
2026-06-11 21:04:22 -0400

    config(nvim): use gruvbox colorscheme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
